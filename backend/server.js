4e4902f5422917bc2ffde47cba9762557702965b
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-06 22:39:37 -0300

    add listen method

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const port = process.env.PORT || 5000;
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
