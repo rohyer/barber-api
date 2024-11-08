793b1ebb5360d3b7be83e962fa60e2aee2010a2e
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-08 09:58:17 -0300

    Develop registerUser method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,51 @@
 const asyncHandler = require("express-async-handler");
+const UserModel = require("../model/userModel");
+const bcrypt = require("bcryptjs");
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password, state, city, phone } = req.body;
+  const { name, email, password, password2, city, state, phone } = req.body;
 
-  if (!name || !email || !password || !state || !city || !phone) {
+  // Verifica se os campos foram preenchidos
+  if (!name || !email || !password || !password2 || !state || !city || !phone) {
     res.status(400);
     throw new Error("Por favor, preencha todos os campos corretamente");
   }
 
-  await res.json({ name, email, password, state, city, phone });
+  // Verifica se o usuário existe
+  const userExists = await UserModel.getUser(email);
+
+  if (userExists.length > 0) {
+    res.status(500);
+    throw new Error("Usuário já existente");
+  }
+
+  // Verifica se as senhas batem e criptografa elas
+  if (password !== password2) {
+    res.status(400);
+    throw new Error("Confirme sua senha digitando-a igualmente");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const passwordHashed = await bcrypt.hash(password, salt);
+
+  // Faz o cadastro do usuário
+  try {
+    const result = await UserModel.addUser(
+      name,
+      email,
+      passwordHashed,
+      city,
+      state,
+      phone
+    );
+    res.status(201);
+    res.json({ message: "Usuário criado", userId: result.insertId });
+  } catch (error) {
+    res.status(500);
+    res.json({ message: "Erro ao criar usuário", error });
+  }
 });
 
-const loginUser = (req, res) => {
-  res.send("Rota de login");
-};
-
 module.exports = {
-  registerUser,
-  loginUser
+  registerUser
 };
