b38d09712e3b8dfd826202dc4d63c910f0405725
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-08 10:35:47 -0300

    Develop loginUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,6 +46,36 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Verifica os dados foram preenchidos
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Por favor, preencha os campos.");
+  }
+
+  // Verifica se o usuário existe
+  const userExists = await UserModel.getUser(email);
+
+  if (userExists.length === 0) {
+    res.status(400);
+    throw new Error("Usuário não encontrado");
+  }
+
+  // Verifica se o usuário existe e se a senha é correta
+  if (
+    userExists.length > 0 &&
+    (await bcrypt.compare(password, userExists[0].password))
+  ) {
+    res.json({ ...userExists[0] });
+  } else {
+    res.status(400);
+    throw new Error("Acessos incorretos");
+  }
+});
+
 module.exports = {
-  registerUser
+  registerUser,
+  loginUser
 };
