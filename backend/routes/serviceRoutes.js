aa818c993edbe620df2f10af6b1dc3078fbe70a7
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-11 20:14:15 -0300

    develop route with PUT HTTP to update service

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
-const { getServices, setService } = require("../controllers/serviceController");
+const {
+  getServices,
+  setService,
+  updateServices
+} = require("../controllers/serviceController");
 
 router.get("/", protect, getServices);
 
 router.post("/", setService);
+
+router.put("/:id", protect, updateServices);
 
 module.exports = router;
