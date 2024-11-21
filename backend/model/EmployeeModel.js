a0691d87b793c7c3051f8933ca3bcb37c95758bd
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-21 16:31:54 -0300

    Develop getEmployeeById and updateEmployee methods

--- backend/model/EmployeeModel.js
+++ backend/model/EmployeeModel.js
@@ -15,6 +15,20 @@ const EmployeeModel = {
     }
   },
 
+  async getEmployeeById(id) {
+    const db = getDatabaseConnection();
+
+    try {
+      const [result] = await db.execute(
+        "SELECT * FROM employee WHERE id = ? LIMIT 1",
+        [id]
+      );
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+
   async setEmployee(name, address, sex, phone, birth, idAdmin) {
     const db = getDatabaseConnection();
 
@@ -22,6 +36,20 @@ const EmployeeModel = {
       const [result] = await db.execute(
         "INSERT INTO employee (name, address, sex, phone, birth, id_admin) VALUES (?, ?, ?, ?, ?, ?)",
         [name, address, sex, phone, birth, idAdmin]
+      );
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async updateEmployee(name, address, sex, phone, birth) {
+    const db = getDatabaseConnection();
+
+    try {
+      const [result, field] = await db.execute(
+        "UPDATE employee SET name = ?, address = ?, sex = ?, phone = ?, birth = ?",
+        [name, address, sex, phone, birth]
       );
       return result;
     } catch (error) {
