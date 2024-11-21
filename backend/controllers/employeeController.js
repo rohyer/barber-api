7b2d20b87af85262a9b1f4210736f34501a3909e
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-21 16:32:21 -0300

    Add comments and develop updatedEmployee

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -1,11 +1,21 @@
 const asyncHandler = require("express-async-handler");
 const EmployeeModel = require("../model/EmployeeModel");
 
+/**
+ * @description Get all employees
+ * @route       GET /api/employees
+ * @access      Private
+ */
 const getEmployees = asyncHandler(async (req, res) => {
   const employees = await EmployeeModel.getEmployees(req.user.id);
   res.status(200).json({ employees });
 });
 
+/**
+ * @description Register employee
+ * @route       POST /api/employees
+ * @access      Private
+ */
 const setEmployee = asyncHandler(async (req, res) => {
   const { name, address, sex, phone, birth } = req.body;
 
@@ -30,7 +40,49 @@ const setEmployee = asyncHandler(async (req, res) => {
   });
 });
 
+/**
+ * @description Update Employee
+ * @route PUT /api/employees/:id
+ * @access Private
+ */
+const updateEmployee = asyncHandler(async (req, res) => {
+  const { name, address, sex, phone, birth } = req.body;
+
+  if (!name || !address || !sex || !phone || !birth) {
+    res.status(400);
+    throw new Error("Por favor preencha os campos");
+  }
+
+  const employeeExists = await EmployeeModel.getEmployeeById(req.params.id);
+
+  if (employeeExists.length === 0) {
+    res.status(400);
+    throw new Error("Colaborador não encontrado!");
+  }
+
+  if (!req.user) {
+    res.status(400);
+    throw new Error("Usuário não encontrado!");
+  }
+
+  if (employeeExists[0].id_admin !== req.user.id) {
+    res.status(400);
+    throw new Error("Usuário não autorizado!");
+  }
+
+  const updatedEmployee = await EmployeeModel.updateEmployee(
+    name,
+    address,
+    sex,
+    phone,
+    birth
+  );
+  res.status(200);
+  res.json(updatedEmployee);
+});
+
 module.exports = {
   getEmployees,
-  setEmployee
+  setEmployee,
+  updateEmployee
 };
