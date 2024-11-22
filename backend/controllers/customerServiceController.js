c897d7ade34a5a5709141ace03b549595aeef5fd
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-22 12:24:05 -0300

    Add comments and develop updateCustomerService

--- backend/controllers/customerServiceController.js
+++ backend/controllers/customerServiceController.js
@@ -1,6 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const CustomerServiceModel = require("../model/CustomerServiceModel");
 
+/**
+ * @description Get all customer services
+ * @route       GET /api/customer-services
+ * @access      Private
+ */
 const getCustomerServices = asyncHandler(async (req, res) => {
   const customerServices = await CustomerServiceModel.getCustomerServices(
     req.params.id
@@ -9,6 +14,11 @@ const getCustomerServices = asyncHandler(async (req, res) => {
   res.status(200).json(customerServices);
 });
 
+/**
+ * @description Register customer service
+ * @route       POST /api/customer-services
+ * @access      Private
+ */
 const setCustomerService = asyncHandler(async (req, res) => {
   const { date, time, idService, idClient, idEmployee } = req.body;
 
@@ -31,6 +41,46 @@ const setCustomerService = asyncHandler(async (req, res) => {
     message: "Cadastro realizado com sucesso!",
     id: result.insertId
   });
+});
+
+/**
+ * @description Update customer service
+ * @route       PUT /api/customer-services/:id
+ * @access      Private
+ */
+const updateCustomerService = asyncHandler(async (req, res) => {
+  const { date, time, idService, idClient, idEmployee } = req.body;
+
+  const customerServiceExists =
+    await CustomerServiceModel.getCustomerServiceById(req.params.id);
+
+  if (customerServiceExists.length === 0) {
+    res.status(400);
+    throw new Error("Atendimento não encontrado!");
+  }
+
+  if (!req.user) {
+    res.status(400);
+    throw new Error("Usuário não encontrado!");
+  }
+
+  if (customerServiceExists[0].id_admin !== req.user.id) {
+    res.status(400);
+    throw new Error("Usuário não autorizado!");
+  }
+
+  const updatedServiceCustomer =
+    await CustomerServiceModel.updateCustomerService(
+      date,
+      time,
+      idService,
+      idClient,
+      idEmployee,
+      req.params.id
+    );
+
+  res.status(200);
+  res.json(updatedServiceCustomer);
 });
 
 module.exports = {
