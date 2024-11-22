fd7c451eea33290f63c25a9f4b518e8324ea208d
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-22 11:35:38 -0300

    Create CustomerServiceModel creating all methods

---
+++ backend/model/CustomerServiceModel.js
@@ -0,0 +1,83 @@
+const getDatabaseConnection = require("../config/db");
+
+const CustomerServiceModel = {
+  async getCustomerServices(idAdmin) {
+    const db = getDatabaseConnection();
+
+    try {
+      const [result] = await db.execute(
+        "SELECT * FROM customer_service WHERE id_admin = ?",
+        [idAdmin]
+      );
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async getCustomerServiceById(id) {
+    const db = getDatabaseConnection();
+
+    try {
+      const [result] = await db.execute(
+        "SELECT * FROM customer_service WHERE id = ? LIMIT 1",
+        [id]
+      );
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async createCustomerService(
+    date,
+    time,
+    status,
+    idService,
+    idClient,
+    idEmployee,
+    idAdmin
+  ) {
+    const db = getDatabaseConnection();
+
+    try {
+      const [result] = await db.execute(
+        "INSERT INTO customer_service (date, time, status, id_service, id_client, id_employee, id_admin) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        [date, time, status, idService, idClient, idEmployee, idAdmin]
+      );
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async updateCustomerService(date, time, status, idService, idEmployee, id) {
+    const db = getDatabaseConnection();
+
+    try {
+      const [result] = await db.execute(
+        "UPDATE customer_service SET name = ?, time = ?, status = ?, id_service = ?, id_employee = ? WHERE id = ?",
+        [date, time, status, idService, idEmployee, id]
+      );
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async deleteCustomerService(id) {
+    const db = getDatabaseConnection();
+
+    try {
+      const [result] = await db.execute(
+        "DELETE FROM customer_service WHERE id = ?",
+        [id]
+      );
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+};
+
+module.exports = CustomerServiceModel;
