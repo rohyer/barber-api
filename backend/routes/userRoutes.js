535a48ca30c09eb6c3cc0c068baa29e65050bee1
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-23 14:21:37 -0300

    Create new put route to update user data

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 
-const { registerUser, loginUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  updateUserData
+} = require("../controllers/userController");
 
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
+
+router.put("/:id", protect, updateUserData);
 
 module.exports = router;
