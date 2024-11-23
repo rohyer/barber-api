e74a2d2eeb34a9eddb709567773ed355a0d4818d
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-23 17:57:55 -0300

    Develop updateUserEmail and confirmEmailChange functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,8 @@ const asyncHandler = require("express-async-handler");
 const UserModel = require("../model/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 
 /**
  * @description Register user
@@ -177,6 +179,120 @@ const updateUserPassword = asyncHandler(async (req, res) => {
   }
 });
 
+/**
+ * @description Update user e-mail
+ * @route       PUT /api/users/email/:id
+ * @access      Private
+ */
+const updateUserEmail = asyncHandler(async (req, res) => {
+  const { newEmail, password } = req.body;
+
+  if (!newEmail || !password) {
+    res.status(400);
+    throw new Error("Por favor, preencha os campos!");
+  }
+
+  if (Number(req.params.id) !== req.user.id) {
+    res.status(400);
+    throw new Error("Usuário não autorizado!");
+  }
+
+  const userExists = await UserModel.getUserById(req.params.id);
+
+  if (userExists.length === 0) {
+    res.status(400);
+    throw new Error("Usuário não encontrado!");
+  }
+
+  const isMatch = await bcrypt.compare(password, userExists[0].password);
+
+  if (!isMatch) {
+    res.status(400);
+    throw new Error("Senha incorreta!");
+  }
+
+  const emailExists = await UserModel.getUser(newEmail);
+
+  if (emailExists.length > 0) {
+    res.status(400);
+    throw new Error("Novo e-mail já está em uso!");
+  }
+
+  // Generate token
+  const token = crypto.randomBytes(32).toString("hex");
+  const tokenExpires = new Date(Date.now() + 3600 * 1000);
+
+  try {
+    // Save request at database
+    await UserModel.saveEmailChangeRequest(
+      newEmail,
+      token,
+      tokenExpires,
+      req.params.id
+    );
+
+    // Setup transporter
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.EMAIL_TRANSPORTER,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+
+    // Send e-mail
+    const link = `http://localhost:5000/confirm-email-change?token=${token}`;
+    await transporter.sendMail({
+      from: process.env.EMAIL_TRANSPORTER,
+      to: newEmail,
+      subject: "Confirmação de alteração de e-mail.",
+      text: `Clique no link para confirmar a alteração de e-mail: ${link}`
+    });
+
+    res.status(200);
+    res.json({ message: "E-mail de confirmação enviado com sucesso!" });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Erro ao solicitar alteração de e-mail!");
+  }
+});
+
+/**
+ * @description Send e-mail to confirm user e-mail change
+ * @route       GET /confirm-email-change
+ * @access      Public
+ */
+const confirmEmailChange = asyncHandler(async (req, res) => {
+  const { token } = req.query;
+
+  if (!token) {
+    res.status(400);
+    throw new Error("Por favor, insira o token!");
+  }
+
+  const user = await UserModel.getUserByEmailToken(token);
+
+  if (!user) {
+    res.status(400);
+    throw new Error("Token inválido ou expirado!");
+  }
+
+  if (user.email_token_expires < new Date()) {
+    res.status(400);
+    throw new Error("Token expirado!");
+  }
+
+  try {
+    await UserModel.updateUserEmail(user.new_email, user.id);
+
+    res.status(200);
+    res.json({ message: "E-mail atualizado com sucesso!" });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Erro ao atualizar e-mail!");
+  }
+});
+
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET);
 };
@@ -185,5 +301,7 @@ module.exports = {
   registerUser,
   loginUser,
   updateUserData,
-  updateUserPassword
+  updateUserPassword,
+  updateUserEmail,
+  confirmEmailChange
 };
