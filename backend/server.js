f55d893d7bc52048dab3902e139e3ed715af034b
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-23 17:58:48 -0300

    Create get route to confirm e-mail change request

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const connectionDB = require("./config/db");
 const port = process.env.PORT || 5000;
+const { confirmEmailChange } = require("./controllers/userController");
 
 connectionDB();
 
@@ -20,5 +21,7 @@ app.use("/api/employees", require("./routes/employeeRoutes"));
 app.use("/api/clients", require("./routes/clientRoutes"));
 
 app.use("/api/customer-services", require("./routes/customerServiceRoutes"));
+
+app.get("/confirm-email-change", confirmEmailChange);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
