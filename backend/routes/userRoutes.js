d2807f0125ed0b1832e08dc35cd2192f720b9da0
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-11-23 15:20:31 -0300

    Create a put route to change user password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,7 +5,8 @@ const { protect } = require("../middleware/authMiddleware");
 const {
   registerUser,
   loginUser,
-  updateUserData
+  updateUserData,
+  updateUserPassword
 } = require("../controllers/userController");
 
 router.post("/register", registerUser);
@@ -13,5 +14,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 router.put("/:id", protect, updateUserData);
+
+router.put("/password/:id", protect, updateUserPassword);
 
 module.exports = router;
