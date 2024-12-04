5ebed1f25c0c62bc17f56810fb04bd6678f8bf9d
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-12-04 11:19:21 -0300

    Create test to getClientById

--- backend/model/ClientModel.spec.js
+++ backend/model/ClientModel.spec.js
@@ -42,4 +42,44 @@ describe("ClientModel", () => {
       ]);
     });
   });
+
+  describe("getClientById", () => {
+    it("Should return only one object", async () => {
+      const mockExecute = jest.fn().mockResolvedValue([
+        [
+          {
+            id: 1,
+            name: "Guilherme R.",
+            sex: "M",
+            phone: "(84) 98105-6717",
+            address: "Rua teste",
+            birth: "2000-01-01",
+            id_admin: 1
+          }
+        ]
+      ]);
+
+      getDatabaseConnection.mockReturnValue({ execute: mockExecute });
+
+      const id = 1;
+
+      const result = await ClientModel.getClientById(id);
+
+      expect(mockExecute).toHaveBeenCalledWith(
+        "SELECT * FROM client WHERE id = ? LIMIT 1",
+        [id]
+      );
+      expect(result).toEqual([
+        {
+          id: 1,
+          name: "Guilherme R.",
+          sex: "M",
+          phone: "(84) 98105-6717",
+          address: "Rua teste",
+          birth: "2000-01-01",
+          id_admin: 1
+        }
+      ]);
+    });
+  });
 });
