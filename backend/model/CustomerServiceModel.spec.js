630312dcc0e37ded48095152545e369eb34c44e3
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-12-05 12:09:15 -0300

    Create tests to updateCustomerService e createCustomerService

--- backend/model/CustomerServiceModel.spec.js
+++ backend/model/CustomerServiceModel.spec.js
@@ -72,7 +72,9 @@ describe("CustomerServicModel", () => {
         }
       ]);
     });
+  });
 
+  describe("getCustomerServiceById", () => {
     it("Should return only one data", async () => {
       const mockExecute = jest.fn().mockResolvedValue([
         [
@@ -116,6 +118,102 @@ describe("CustomerServicModel", () => {
           updated_at: "2024-12-05T14:21:00.000Z"
         }
       ]);
+    });
+  });
+
+  describe("createCustomerService", () => {
+    it("should create one customer service", async () => {
+      const mockExecute = jest.fn().mockResolvedValue([
+        {
+          fieldCount: 0,
+          affectedRows: 1,
+          insertId: 5,
+          info: "",
+          serverStatus: 2,
+          warningStatus: 0,
+          changedRows: 0
+        }
+      ]);
+
+      getConnectionDatabase.mockReturnValue({ execute: mockExecute });
+
+      const date = "2024-12-10";
+      const time = "14:00:00";
+      const idService = 1;
+      const idClient = 1;
+      const idEmployee = 5;
+      const idAdmin = 1;
+
+      const result = await CustomerServiceModel.createCustomerService(
+        date,
+        time,
+        idService,
+        idClient,
+        idEmployee,
+        idAdmin
+      );
+
+      expect(mockExecute).toHaveBeenCalledWith(
+        "INSERT INTO customer_service (date, time, id_service, id_client, id_employee, id_admin) VALUES (?, ?, ?, ?, ?, ?)",
+        [date, time, idService, idClient, idEmployee, idAdmin]
+      );
+      expect(result).toEqual({
+        fieldCount: 0,
+        affectedRows: 1,
+        insertId: 5,
+        info: "",
+        serverStatus: 2,
+        warningStatus: 0,
+        changedRows: 0
+      });
+    });
+  });
+
+  describe("updateCustomerService", () => {
+    it("should update one customer service", async () => {
+      const mockExecute = jest.fn().mockResolvedValue([
+        {
+          fieldCount: 0,
+          affectedRows: 1,
+          insertId: 0,
+          info: "Rows matched: 1  Changed: 0  Warnings: 0",
+          serverStatus: 2,
+          warningStatus: 0,
+          changedRows: 0
+        }
+      ]);
+
+      getConnectionDatabase.mockReturnValue({ execute: mockExecute });
+
+      const date = "2024-12-10";
+      const time = "14:00:00";
+      const status = "open";
+      const idService = 1;
+      const idEmployee = 5;
+      const id = 1;
+
+      const result = await CustomerServiceModel.updateCustomerService(
+        date,
+        time,
+        status,
+        idService,
+        idEmployee,
+        id
+      );
+
+      expect(mockExecute).toHaveBeenCalledWith(
+        "UPDATE customer_service SET date = ?, time = ?, status = ?, id_service = ?, id_employee = ? WHERE id = ?",
+        [date, time, status, idService, idEmployee, id]
+      );
+      expect(result).toEqual({
+        fieldCount: 0,
+        affectedRows: 1,
+        insertId: 0,
+        info: "Rows matched: 1  Changed: 0  Warnings: 0",
+        serverStatus: 2,
+        warningStatus: 0,
+        changedRows: 0
+      });
     });
   });
 });
