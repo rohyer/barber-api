857a76f795381a181535dd89d9367373ce265710
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-01-25 15:51:50 -0300

    Develop (#3)
    * Change env variables
    
    * Change node image
    
    * Change log of listen method
    
    * Update docker-compose add app instances, nginx and importing env variables
    
    * Develop a deploy workflow
    
    * Change variables
    
    * Delete init.sql
    
    * Remove init.sql
    
    * Add init.sql to gitignore
    
    * Fix workflow
    
    * Fix workflow
    
    * Fix workflow
    
    * Remove init.sql from gitignore
    
    * Add init.sql
    
    * Update docker-compose creating no-root database user
    
    * Install nginx
    
    * Remove secrets
    
    * Test code
    
    * Change workflow
    
    * Add secrets
    
    * Add environment
    
    * Fix docker command
    
    * Remove env_file
    
    * Change workflow
    
    * Chage workflow
    
    * Add --env-file
    
    * Fix variables
    
    * Change variables
    
    * Add new step to workflow
    
    * Change order
    
    * Remove .env
    
    * Remove env files
    
    * Create deploy-development and deploy-production workflows
    
    * Delete deploy workflow

---
+++ run.sh
@@ -0,0 +1,3 @@
+docker run -e NODE_ENV=development -e PORT=5000 -e JWT_SECRET=bn56tb -e EMAIL_TRANSPORTER=guilhermero.chatgpt@gmail.com -e EMAIL_PASSWORD=ofpe pyoo ohpo yodb -p 5000:5000 --name barber-api-container rohyer/barber-api
+
+docker run -e NODE_ENV=development -e PORT=5000 -e JWT_SECRET=bn56tb -p 5000:5000 --name barber-api-container rohyer/barber-api
