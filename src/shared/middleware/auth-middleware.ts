c11a44e6a31e1809f0a1d64ea039c2c72b89080a
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-06-22 16:29:12 -0300

    fix: verify if result exists #12

--- src/shared/middleware/auth-middleware.ts
+++ src/shared/middleware/auth-middleware.ts
@@ -4,32 +4,31 @@ import UserModel from "../../modules/user/user.model.js";
 import { JwtPayload } from "jsonwebtoken";
 import { AuthenticatedRequest, IUserPayload } from "../types/express.type.js";
 
-export const protect = asyncHandler(
-  async (req: AuthenticatedRequest, res, next) => {
-    let token;
+export const protect = asyncHandler(async (req: AuthenticatedRequest, res, next) => {
+  let token;
 
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      try {
-        token = req.headers.authorization.split(" ")[1];
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
 
-        if (!process.env.JWT_SECRET)
-          throw new Error("JWT_SECRET não definido!");
+      if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET não definido!");
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
 
-        const result = await UserModel.getUserById(decoded.id);
+      const result = await UserModel.getUserById(decoded.id);
 
-        req.user = result[0] as IUserPayload;
-
-        next();
-      } catch (error) {
-        throw new Error("Não autorizado", { cause: error });
+      if (!result || result.length === 0) {
+        res.status(401);
+        throw new Error("Usuário não encontrado");
       }
-    } else {
-      throw new Error("Sem autorização");
+
+      req.user = result[0] as IUserPayload;
+
+      next();
+    } catch (error) {
+      throw new Error("Não autorizado", { cause: error });
     }
-  },
-);
+  } else {
+    throw new Error("Sem autorização");
+  }
+});
