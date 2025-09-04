fcfbc47d6568aec977dde1d2b4c092edf9205bc7
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-09-03 23:35:41 -0300

    feat: create successHandler util function

---
+++ src/shared/utils/successHandler.ts
@@ -0,0 +1,19 @@
+import { Response as ExpressResponse } from "express";
+
+export type SuccessHandler = {
+    status: number;
+    success: boolean;
+    message: string;
+    data: any;
+};
+
+export const successHandler = (
+    res: ExpressResponse,
+    { data, message, status = 200 }: SuccessHandler,
+) => {
+    res.status(status).json({
+        success: true,
+        message,
+        data,
+    });
+};
