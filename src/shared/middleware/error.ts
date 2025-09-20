9a5e5f396a5ca219c539b36c0325e67798590f5a
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-09-20 00:29:54 -0300

    Add type guard

--- src/shared/middleware/error.ts
+++ src/shared/middleware/error.ts
@@ -8,6 +8,8 @@ export const errorHandler = (
     res: ExpressResponse,
     _next: NextFunction,
 ) => {
+    if (res.headersSent) return;
+
     const status = error.status || 500;
 
     res.status(status).json({
