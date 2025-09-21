5d6659bc232b5224aa1fb24498625ee1e201fc8d
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-09-20 21:32:56 -0300

    Add fromCache property in the successHandler

--- src/shared/utils/successHandler.ts
+++ src/shared/utils/successHandler.ts
@@ -3,16 +3,18 @@ import { Response as ExpressResponse } from "express";
 export type SuccessHandler = {
     status: number;
     message: string;
+    fromCache: boolean;
     data: any;
 };
 
 export const successHandler = (
     res: ExpressResponse,
-    { data, message, status = 200 }: SuccessHandler,
+    { data, message, fromCache, status = 200 }: SuccessHandler,
 ) => {
     res.status(status).json({
         success: true,
         message,
+        fromCache,
         data,
     });
 };
