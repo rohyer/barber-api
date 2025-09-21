6ff7aa219adaa02551b70097afa3dcb120b2c0fd
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-09-20 21:32:27 -0300

    Improve cache middleware to accept prefix paramenter and define offset and query values to redis cacheKey

--- src/shared/middleware/cache.ts
+++ src/shared/middleware/cache.ts
@@ -3,36 +3,36 @@ import { AuthenticatedRequest } from "../types/express.type.js";
 import { Response as ExpressResponse, NextFunction } from "express";
 import { successHandler } from "../utils/successHandler.js";
 
-export const cacheMiddleware = async (
-    req: AuthenticatedRequest,
-    res: ExpressResponse,
-    next: NextFunction,
-) => {
-    if (!req.user) {
-        res.status(401);
-        throw new Error("Usuário não autenticado!");
-    }
-
-    // const id = req.params.id;
-    const prefix = req.originalUrl.split("/");
-    const cacheKey = `${prefix[prefix.length - 1]}:user:${req.user.id}`;
-
-    try {
-        const cachedData = await redisClient.get(cacheKey);
-
-        req.cacheKey = cacheKey;
-
-        if (cachedData) {
-            successHandler(res, {
-                status: 200,
-                message: "Requisição feita com sucesso",
-                data: JSON.parse(cachedData),
-            });
+export const cacheMiddleware = (prefix: string) => {
+    return async (req: AuthenticatedRequest, res: ExpressResponse, next: NextFunction) => {
+        if (!req.user) {
+            res.status(401);
+            throw new Error("Usuário não autenticado!");
         }
 
-        next();
-    } catch (error) {
-        console.error(`Erro ao verificar o cache ${prefix}`, error);
-        next();
-    }
+        const offset = req.query.offset ?? 0;
+        const query = req.query.query ?? "";
+
+        const cacheKey = `${prefix}:user:${req.user.id}:offset:${offset}:query:${query}`;
+
+        try {
+            const cachedData = await redisClient.get(cacheKey);
+
+            req.cacheKey = cacheKey;
+
+            if (cachedData) {
+                successHandler(res, {
+                    status: 200,
+                    message: "Requisição feita com sucesso CACHE",
+                    fromCache: true,
+                    data: JSON.parse(cachedData),
+                });
+            }
+
+            next();
+        } catch (error) {
+            console.error(`Erro ao verificar o cache ${prefix}`, error);
+            next();
+        }
+    };
 };
