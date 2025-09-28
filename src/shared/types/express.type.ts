e66321991d8332c8bb0f3c1a413c45dce3417d49
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2025-09-27 22:59:06 -0300

    Improve AuthenticatedRequest adding generics parameters

--- src/shared/types/express.type.ts
+++ src/shared/types/express.type.ts
@@ -1,10 +1,16 @@
 import { Request as ExpressRequest } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
 
 export interface IUserPayload {
     id: number;
 }
 
-export interface AuthenticatedRequest extends ExpressRequest {
+export interface AuthenticatedRequest<
+    Params = ParamsDictionary,
+    ResBody = any,
+    ReqBody = any,
+    ReqQuery = any,
+> extends ExpressRequest<Params, ResBody, ReqBody, ReqQuery> {
     user?: IUserPayload;
     cacheKey?: string;
 }
