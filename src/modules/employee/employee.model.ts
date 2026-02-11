56f47c5b716fe7bd2cab8ba93f7f4db6963b8adf
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-02-10 22:25:13 -0300

    Improve query to get all necessary data

--- src/modules/employee/employee.model.ts
+++ src/modules/employee/employee.model.ts
@@ -4,14 +4,25 @@ import redisClient from "../../shared/config/redis-client.js";
 import { IEmployee } from "./employee.type.js";
 
 const EmployeeModel = {
-    async getEmployees(idAdmin: IEmployee["idAdmin"]) {
+    async getEmployees(idAdmin: IEmployee["idAdmin"], offset: number, query: string) {
         const db = getDatabaseConnection();
 
         const [result] = await db.execute<(IEmployee & RowDataPacket)[]>(
-            "SELECT id, name, address, sex, phone, birth FROM employee WHERE id_admin = ?",
-            [idAdmin],
+            "SELECT e.id, e.name, e.address, e.sex, e.phone, e.birth, COUNT(cs.id) as totalAppointments, e.created_at AS createdAT FROM employee e LEFT JOIN customer_service cs ON e.id = cs.id_employee WHERE e.id_admin = ? AND (? = '' OR e.name LIKE CONCAT('%', ?, '%')) GROUP BY e.id, e.name, e.address, e.sex, e.phone, e.birth, e.created_at ORDER BY e.id DESC LIMIT 10 OFFSET ?",
+            [idAdmin, query, query, offset],
         );
-        return result;
+
+        const [rows] = await db.execute<RowDataPacket[]>(
+            "SELECT COUNT(*) AS total FROM employee WHERE id_admin = ? AND (? = '' OR name LIKE CONCAT('%', ?, '%'))",
+            [idAdmin, query, query],
+        );
+
+        const total = rows[0].total as number;
+
+        return {
+            employees: result,
+            total,
+        };
     },
 
     async getEmployeeById(id: IEmployee["id"]) {
