da196f395ed4aecf790bdb3e9c82b2f99fcec1fe
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-02-19 22:33:09 -0300

    Chore: Remove prettier and install eslint stylistic

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,9 @@ import globals from "globals";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import { config as tseslintConfig } from "typescript-eslint";
 import parser from "@typescript-eslint/parser";
+import stylistic from "@stylistic/eslint-plugin";
 
 import { defineConfig } from "eslint/config";
-import prettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export default defineConfig([
     ...tseslintConfig(),
@@ -17,8 +16,8 @@ export default defineConfig([
         files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
         plugins: {
             js,
-            prettier: eslintPluginPrettier,
             "@typescript-eslint": tseslint,
+            "@stylistic": stylistic,
         },
         languageOptions: {
             parser,
@@ -29,23 +28,37 @@ export default defineConfig([
             globals: globals.node,
         },
         rules: {
-            ...prettier.rules,
-
-            "prettier/prettier": [
-                "warn",
-                {
-                    printWidth: 100,
-                    semi: true,
-                    tabWidth: 4,
-                    bracketSpacing: true,
-                    endOfLine: "lf",
-                },
-            ],
             "@typescript-eslint/no-var-requires": "off",
+
             "@typescript-eslint/no-require-imports": "error", // <- esse é o que está dando erro
+
             "no-undef": "off", // evita 'require', 'process' como indefinidos
             "no-unused-vars": "off",
             "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+            
+            "curly": ["error", "multi-or-nest"],
+            "nonblock-statement-body-position": ["error", "below"],
+
+            "@stylistic/semi": ["error", "always"],
+            "@stylistic/indent": ["error", 4, { SwitchCase: 1 }],
+            "@stylistic/object-curly-spacing": ["error", "always"],
+            "@stylistic/quotes": ["error", "double", { "avoidEscape": true }],
+            "@stylistic/comma-dangle": ["error", "always-multiline"],
+            "@stylistic/arrow-parens": ["error", "as-needed"],
+            "@stylistic/no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+            "@stylistic/spaced-comment": ["error", "always"],
+            "@stylistic/function-paren-newline": ["error", "consistent"],
+            "@stylistic/member-delimiter-style": [
+                "error", {
+                    "multiline": { "delimiter": "semi", "requireLast": true },
+                    "singleline": { "delimiter": "semi", "requireLast": false },
+                },
+            ],
+            "@stylistic/array-element-newline": ["error", "consistent"],
+            "@stylistic/array-bracket-newline": ["error", { "multiline": true }],
+            "@stylistic/object-property-newline": ["error", { "allowAllPropertiesOnSameLine": true }],
+            "@stylistic/function-call-argument-newline": ["error", "consistent"],
+            "@stylistic/linebreak-style": ["error", "unix"],
         },
     },
     { ignores: ["node_modules", "build", "dist"] },
