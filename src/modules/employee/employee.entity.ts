0570946fb187754b088acdf12ca40d3b27ac59f5
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-04 22:34:19 -0300

    Create EmployeeEntity

---
+++ src/modules/employee/employee.entity.ts
@@ -0,0 +1,46 @@
+export type EmployeeEntityProps = {
+    id?: number;
+    name: string;
+    sex: "M" | "F" | "Outro";
+    phone: string;
+    address: string;
+    birth: string;
+    idAdmin: number;
+    createdAt?: string;
+    updatedAt?: string;
+    lastCustomerServiceDate?: string | null;
+};
+
+export class EmployeeEntity {
+    private props: EmployeeEntityProps;
+
+    constructor(props: EmployeeEntityProps) {
+        this.props = props;
+    }
+
+    get data() {
+        return { ...this.props };
+    }
+
+    static createFromDatabase(row: any): EmployeeEntity {
+        return new EmployeeEntity({
+            id: row.id,
+            name: row.name,
+            sex: row.sex,
+            phone: row.phone,
+            address: row.address,
+            birth: row.birth,
+            idAdmin: row.idAdmin,
+            createdAt: row.createdAt,
+            updatedAt: row.updatedAt,
+            lastCustomerServiceDate: row.lastCustomerServiceDate,
+        });
+    }
+
+    public update(data: Pick<EmployeeEntityProps, "name" | "sex" | "phone" | "address" | "birth">) {
+        this.props = {
+            ...this.props,
+            ...data,
+        };
+    }
+}
