1af53357ff2f249d66b5e469b6447cbd55b19ab9
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-07 23:00:42 -0300

    Develop CreateEmployeeService type to be used in service layer

--- src/modules/employee/service/employee.service.type.ts
+++ src/modules/employee/service/employee.service.type.ts
@@ -1,6 +1,11 @@
-import { GetEmployeeDTO } from "../employee.dto.js";
+import { CreateEmployeeDTO, GetEmployeeDTO } from "../employee.dto.js";
 
 export type GetEmployeeService = GetEmployeeDTO & {
+    idAdmin: number;
+    cacheKey: string | undefined;
+};
+
+export type CreateEmployeeService = CreateEmployeeDTO & {
     idAdmin: number;
     cacheKey: string | undefined;
 };
