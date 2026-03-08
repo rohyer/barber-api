0e9caa6fd8178ab01d879a40b5c91ec5f22ea171
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-07 23:51:10 -0300

    Create findEmployeesById and updateEmployee functions and change all functions to arrow functions

--- src/modules/employee/repository/employee.repository.ts
+++ src/modules/employee/repository/employee.repository.ts
@@ -5,7 +5,7 @@ import { EmployeeEntity, EmployeeEntityProps } from "../employee.entity.js";
 export class EmployeeRepository {
     constructor(private readonly db: Pool) {};
 
-    async findAllEmployees ({ idAdmin, offset, query }: FindAllEmployeesParams): Promise<FindAllEmployeesResponse | null> {
+    findAllEmployees = async ({ idAdmin, offset, query }: FindAllEmployeesParams): Promise<FindAllEmployeesResponse | null> => {
         const [employeeRows] = await this.db.execute<(EmployeeEntityProps & RowDataPacket)[]>(
             "SELECT e.id, e.name, e.address, e.sex, e.phone, e.birth, COUNT(cs.id) as totalAppointments, e.created_at AS createdAT FROM employee e LEFT JOIN customer_service cs ON e.id = cs.id_employee WHERE e.id_admin = ? AND (? = '' OR e.name LIKE CONCAT('%', ?, '%')) GROUP BY e.id, e.name, e.address, e.sex, e.phone, e.birth, e.created_at ORDER BY e.id DESC LIMIT 10 OFFSET ?",
             [idAdmin, query, query, offset],
@@ -26,7 +26,21 @@ export class EmployeeRepository {
         };
     };
 
-    async createEmployee(employee: EmployeeEntity): Promise<EmployeeEntity | null> {
+    findEmployeeById = async (id: EmployeeEntityProps["id"]): Promise<EmployeeEntity | null> => {
+        const [employeeRows] = await this.db.execute<(EmployeeEntityProps & RowDataPacket)[]>(
+            "SELECT id, name, address, sex, phone, birth, id_admin AS idAdmin FROM employee WHERE id = ? LIMIT 1",
+            [id],
+        );
+
+        if (employeeRows.length !== 0)
+            return null;
+
+        const createdEmployee = EmployeeEntity.createFromDatabase(employeeRows[0]);
+
+        return createdEmployee;
+    };
+
+    createEmployee = async (employee: EmployeeEntity): Promise<EmployeeEntity | null> =>  {
         const { idAdmin, name, address, sex, phone, birth } = employee.data;
 
         const [result] = await this.db.execute<ResultSetHeader>(
@@ -37,5 +51,21 @@ export class EmployeeRepository {
         const createdEmployee = EmployeeEntity.createFromDatabase({ ...employee.data, id: result.insertId });
 
         return createdEmployee;
-    }
+    };
+
+    updateEmployee = async (employee: EmployeeEntity): Promise<EmployeeEntity | null> => {
+        const { name, sex, phone, address, birth, id } = employee.data;
+
+        const [result] = await this.db.execute<ResultSetHeader>(
+            "UPDATE client SET name = ?, sex = ?, phone = ?, address = ?, birth = ? WHERE id = ?",
+            [name, sex, phone, address, birth, id],
+        );
+
+        if (result.affectedRows === 0)
+            return null;
+
+        const updatedEmployee = EmployeeEntity.createFromDatabase({ ...employee.data });
+
+        return updatedEmployee;
+    };
 }
