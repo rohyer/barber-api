213d05eacc18e9f006bf5c8b90ffb6cdf677d295
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-07 23:00:08 -0300

    Create employee schema and dto

--- src/modules/employee/employee.dto.ts
+++ src/modules/employee/employee.dto.ts
@@ -5,4 +5,15 @@ export const GetEmployeeSchema = z.object({
     query: z.string().optional().default(""),
 });
 
+export const CreateEmployeeSchema = z.object({
+    name: z.string().min(1, "Preencha o campo nome"),
+    address: z.string().min(1, "Preencha o campo endereço"),
+    sex: z.enum(["M", "F", "Outro"]),
+    phone: z.string().min(1, "Preencha o campo telefone"),
+    birth: z.string().min(1, "Preencha o campo data de nascimento"),
+
+});
+
 export type GetEmployeeDTO = z.infer<typeof GetEmployeeSchema>;
+
+export type CreateEmployeeDTO = z.infer<typeof CreateEmployeeSchema>;
