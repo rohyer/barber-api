264144d3a32c36f34584a68333373fd97c9c25ff
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-09 22:03:32 -0300

    Create offering schemas and DTOs

---
+++ src/modules/service/offering.dto.ts
@@ -0,0 +1,15 @@
+import z, { number, string } from "zod";
+
+export const GetOfferingSchema = z.object({
+    page: z.coerce.number().min(1).default(1),
+    query: z.string().optional().default(""),
+});
+
+export const CreateOfferingSchema = z.object({
+    name: string().min(1, "Insira um nome válido"),
+    value: number().min(1, "Insira um valor válido"),
+});
+
+export type GetOfferingDTO = z.infer<typeof GetOfferingSchema>;
+
+export type CreateOfferingDTO = z.infer<typeof CreateOfferingSchema>;
