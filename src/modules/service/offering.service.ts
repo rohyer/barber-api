8ef4ed6317ce5b453b49fb559895ce309dd3f018
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-10 22:13:52 -0300

    Create regiter and update service

--- src/modules/service/offering.service.ts
+++ src/modules/service/offering.service.ts
@@ -1,10 +1,21 @@
 import redisClient from "../../shared/config/redis-client.js";
-import { GetOfferingDTO } from "./offering.dto.js";
+import { CreateOfferingDTO, GetOfferingDTO, UpdateOfferingDTO } from "./offering.dto.js";
+import { OfferingEntity } from "./offering.entity.js";
 import { OfferingRepository } from "./offering.repository.js";
 
 type GetOfferingService = GetOfferingDTO & {
     idAdmin: number;
     cacheKey: string | undefined;
+};
+
+type RegisterOfferingService = CreateOfferingDTO & {
+    idAdmin: number;
+    cacheKey: string | undefined;
+};
+
+type UpdateOfferingService = UpdateOfferingDTO & {
+    idAdmin: number;
+    id: number;
 };
 
 export class OfferingService {
@@ -33,5 +44,55 @@ export class OfferingService {
             redisClient.set(data.cacheKey, JSON.stringify(result), { EX: 300 });
 
         return result;
+    };
+
+    registerOffering = async(data: RegisterOfferingService) => {
+        const offeringExists = await this.offeringRepository.getServiceByName(data.name);
+
+        if (offeringExists.length > 0)
+            throw new Error("Serviço já existente");
+
+        const offering = new OfferingEntity({ ...data });
+
+        const queryResult = await this.offeringRepository.createService(offering);
+
+        if (!queryResult || !queryResult.data.id)
+            throw new Error("Erro ao salvar serviço");
+
+        const cacheKeys = await redisClient.keys(`offering:user:${data.idAdmin}:*`);
+
+        if (cacheKeys.length > 0)
+            await redisClient.del(cacheKeys);
+
+        return queryResult;
+    };
+
+    updateOffering = async(data: UpdateOfferingService) => {
+        const offering = await this.offeringRepository.getServiceById(data.id);
+
+        if (!offering || !offering.data.id)
+            throw new Error("Serviço não encontrado");
+
+        if (data.idAdmin !== offering.data.idAdmin)
+            throw new Error("Usuário não autorizado");
+
+        offering.update({
+            name: data.name,
+            value: data.value,
+        });
+
+        const updatedOffering = await this.offeringRepository.updateService(offering);
+
+        if (!updatedOffering)
+            return null;
+
+        const cacheKeys = await redisClient.keys(`offering:user:${data.idAdmin}:*`);
+
+        if (cacheKeys.length > 0) 
+            await redisClient.del(cacheKeys);
+
+        return {
+            data: updatedOffering,
+        };
     };
 }
