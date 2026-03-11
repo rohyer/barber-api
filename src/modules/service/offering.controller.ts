1eb270c72766f09c91deebcdd23d2dfdeabcfcd2
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-03-10 22:11:56 -0300

    Develop create and update controllers

--- src/modules/service/offering.controller.ts
+++ src/modules/service/offering.controller.ts
@@ -1,5 +1,4 @@
 import asyncHandler from "express-async-handler";
-import ServiceModel from "./offering.repository.js";
 import { AuthenticatedRequest } from "../../shared/types/express.type.js";
 import { Response as ExpressResponse } from "express";
 import { successHandler } from "../../shared/utils/successHandler.js";
@@ -8,11 +7,6 @@ import { OfferingService } from "./offering.service.js";
 export class OfferingController {
     constructor(private offeringService: OfferingService) {};
 
-    /**
-     * @description Get services
-     * @route       GET /api/services
-     * @access      Private
-     */
     getOfferings = asyncHandler(async (req: AuthenticatedRequest, res: ExpressResponse) => {
         const data = await this.offeringService.getOfferings({
             ...req.query,
@@ -29,109 +23,59 @@ export class OfferingController {
         
         return successHandler(res, responseData);
     });
-}
 
-/**
- * @description Set services
- * @route       POST /api/services
- * @access      Private
- */
-// Precisar passar no protect? req.user.id?
-export const setService = asyncHandler(async (req: AuthenticatedRequest, res: ExpressResponse) => {
-    if (!req.user) {
-        res.status(401);
-        throw new Error("Usuário não autenticado");
-    }
+    registerOffering = asyncHandler(async (req: AuthenticatedRequest, res: ExpressResponse) => {
+        const data = await this.offeringService.registerOffering({
+            ...req.body,
+            idAdmin: req.user!.id,
+        });
 
-    const { name, value } = req.body;
-
-    // Verifica se os campos foram preenchidos
-    if (!name || !value) {
-        res.status(400);
-        throw new Error("Por favor, preencha os campos corretamente");
-    }
-
-    // Verifica se o serviço já existe
-    const serviceExists = await ServiceModel.getServiceByName(name);
-
-    if (serviceExists.length > 0) {
-        res.status(400);
-        throw new Error("Serviço já existe");
-    }
-
-    const serviceData = { name, value, idAdmin: req.user.id };
-
-    // Faz o cadastro no Banco de Dados
-    const result = await ServiceModel.createService(serviceData);
-
-    res.status(201).json({
-        message: "Serviço criado",
-        id: result.insertId,
-    });
-});
-
-/**
- * @description Update services
- * @route       PUT /api/services/:id
- * @access      Private
- */
-export const updateService = asyncHandler(
-    async (req: AuthenticatedRequest, res: ExpressResponse) => {
-        if (!req.user) {
-            res.status(400);
-            throw new Error("Usuário não encontrado");
-        }
-
-        const { name, value } = req.body;
-
-        if (!name || !value) {
-            res.status(400);
-            throw new Error("Por favor, preencha os campos corretamente");
-        }
-
-        const service = await ServiceModel.getServiceById(req.params.id);
-
-        if (service.length === 0) {
-            res.status(400);
-            throw new Error("Serviço não encontrado");
-        }
-
-        if (service[0].id_admin !== req.user.id) {
-            res.status(400);
-            throw new Error("Usuário não autorizado");
-        }
-
-        const serviceData = {
-            name,
-            value,
-            id: Number(req.params.id),
-            idAdmin: req.user.id,
+        const responseData = {
+            status: 201,
+            message: "Serviço cadastrado com sucesso.",
+            fromCache: false,
+            data,
         };
 
-        const result = await ServiceModel.updateService(serviceData);
+        return successHandler(res, responseData);
+    });
 
-        res.status(200);
-        res.json({
-            success: true,
-            message: "Serviço atualizado com sucesso!",
-            data: {
-                affectedRows: result.affectedRows,
-            },
+    updateService = asyncHandler(async (req: AuthenticatedRequest, res: ExpressResponse) => {
+        const data = await this.offeringService.updateOffering({
+            ...req.body,
+            id: req.params.id,
+            idAdmin: req.user!.id,
         });
-    },
-);
 
-/**
- * @description Delete services
- * @route       DELETE /api/services/:id
- * @access      Private
- */
-export const deleteService = asyncHandler(
-    async (req: AuthenticatedRequest, res: ExpressResponse) => {
-        if (!req.user) {
-            res.status(400);
-            throw new Error("Usuário não encontrado");
-        }
+        const responseData = {
+            status: 200,
+            message: "Serviço editado com sucesso",
+            fromCache: false,
+            data,
+        };
+
+        return successHandler(res, responseData);
+    });
+
+    deleteService = asyncHandler(async (req: AuthenticatedRequest, res: ExpressResponse) => {
+        const isDeletedClient = await this.offeringService.deleteOffering({
+            id: req.params.id,
+            idAdmin: req.user!.id,
+        });
+
+        if (!isDeletedClient)
+            throw new Error("Erro ao deletar serviço");
+
+        const responseData = {
+            status: 200,
+            message: "Serviço deletado com sucesso",
+            fromCache: false,
+            data: {
+                id: req.params.id,
+            },
+        };
+
+        return successHandler(res, responseData);
 
         const service = await ServiceModel.getServiceById(req.params.id);
 
@@ -155,5 +99,6 @@ export const deleteService = asyncHandler(
                 affectedRows: deletedService.affectedRows,
             },
         });
-    },
-);
+    });
+
+}
