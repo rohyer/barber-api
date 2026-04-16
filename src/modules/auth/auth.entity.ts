4e9e772cc8374bf4723c4c7b9d10d21bf3a0c6ae
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-15 23:43:58 -0300

    initial development of auth.entity

---
+++ src/modules/auth/auth.entity.ts
@@ -0,0 +1,27 @@
+type AuthEntityProps = {
+    name: string;
+    email: string;
+    password: string;
+    city: string;
+    state: string;
+    phone: string;
+};
+
+export class AuthEntity {
+    private props: AuthEntityProps;
+
+    constructor(props: AuthEntityProps) {
+        this.props = { ...props };
+    }
+
+    static createFromDatabase(row: any): AuthEntity {
+        return new AuthEntity({
+            name: row.name,
+            email: row.email,
+            password: row.password,
+            city: row.city,
+            state: row.state,
+            phone: row.phone,
+        });
+    }
+}
