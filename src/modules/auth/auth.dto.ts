c59acb331f2f5762025182768b9330d6c4061b42
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-16 23:14:54 -0300

    feat: add password validate in schema

--- src/modules/auth/auth.dto.ts
+++ src/modules/auth/auth.dto.ts
@@ -8,6 +8,9 @@ export const registerBarbeShopSchema = z.object({
     city: z.string(),
     state: z.string(),
     phone: z.string(),
+}).refine(data => data.password === data.passwordConfirm, {
+    error: "A confirmação de senha deve ser igual à senha digitada",
+    path: ["passwordConfirm"],
 });
 
 export type RegisterBarberShop = z.infer<typeof registerBarbeShopSchema>;
