310a02094a4d17d7dfa4d21c482ac1e91bd0e519
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-17 00:40:13 -0300

    initial development of auth.repository

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,27 +1,39 @@
 import { Pool, ResultSetHeader, RowDataPacket } from "mysql2/promise";
 import { RegisterBarberShop } from "./auth.dto.js";
+import { AuthEntity, AuthEntityProps } from "./auth.entity.js";
 
 export class AuthRepository {
     constructor(private readonly db: Pool) {}
 
-    getUserByEmail = async (email: string): Promise<RowDataPacket[]> => {
+    isEmailAvailable = async (email: string): Promise<boolean> => {
         const query = "SELECT id FROM admin WHERE email = ? LIMIT 1";
 
-        const [rows] = await this.db.execute<RowDataPacket[]>(query, [email]);
-
-        return rows;
-    };
-
-    insertBarbershop = (barbershopData: RegisterBarberShop) => {
-        const { name, email, password, passwordConfirm, phone, city, state } = barbershopData;
-
-        const query = "INSERT INTO admin (name, email, password, passwordConfirm, phone, city, state) VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-        const result = this.db.execute<ResultSetHeader>(
+        const [rows] = await this.db.execute<AuthEntityProps["id"] & RowDataPacket[]>(
             query,
-            [name, email, password, passwordConfirm, phone, city, state],
+            [email],
         );
 
-        return result;
+        if (rows.length === 0)
+            return true;
+
+        return false;
+    };
+
+    insertBarbershop = async (barbershopData: RegisterBarberShop): Promise<AuthEntity | null> => {
+        const { name, email, password, phone, city, state } = barbershopData;
+
+        const query = "INSERT INTO admin (name, email, password, phone, city, state) VALUES (?, ?, ?, ?, ?, ?)";
+
+        const [result] = await this.db.execute<ResultSetHeader>(
+            query,
+            [name, email, password, phone, city, state],
+        );
+
+        if (!result.insertId)
+            return null;
+
+        const entity = AuthEntity.createFromDatabase({ name, email, password, phone, city, state });
+
+        return entity;
     };
 }
