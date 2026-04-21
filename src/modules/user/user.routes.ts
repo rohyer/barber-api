32be7534193d105203573c1a464b367055714e34
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-21 12:39:12 -0300

    Remove auth routes from user route

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -2,16 +2,10 @@ import express from "express";
 const router = express.Router();
 import { protect } from "../../shared/middleware/auth.js";
 import {
-    registerUser,
-    loginUser,
     updateUserData,
     updateUserPassword,
     updateUserEmail,
 } from "./user.controller.js";
-
-router.post("/register", registerUser);
-
-router.post("/login", loginUser);
 
 router.put("/:id", protect, updateUserData);
 
