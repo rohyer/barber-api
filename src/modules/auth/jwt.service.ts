5bbd5cbc9fb661acd9542f8d184054ade721346d
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-21 22:39:44 -0300

    develop JwtService

---
+++ src/modules/auth/jwt.service.ts
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export class JwtService {
+    generateToken = (barbershopId: number) => {
+        if (!process.env.JWT_SECRET) 
+            throw new Error("Variável de ambiente não definida");
+    
+        return jwt.sign(
+            barbershopId.toString(),
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" },
+        );
+    };
+    
+}
