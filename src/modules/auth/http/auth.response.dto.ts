fb74c8ba20d4227bf95ea80cd4aef353918dddd5
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-25 00:13:14 -0300

    fix response dto

--- src/modules/auth/http/auth.response.dto.ts
+++ src/modules/auth/http/auth.response.dto.ts
@@ -1,11 +1,8 @@
 export type LoginResponse = {
-    token: string;
-    data: {
-        id: number;
-        name: string;
-        email: string;
-        city: string;
-        state: string;
-        phone: string;
-    };
+    id: number;
+    name: string;
+    email: string;
+    city: string;
+    state: string;
+    phone: string;
 };
