141eb6250f71be619179cee7df7bd6863f871d8a
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-25 00:12:59 -0300

    develop authMe middleware

---
+++ src/modules/auth/auth.middleware.ts
@@ -0,0 +1,29 @@
+import asyncHandler from "express-async-handler";
+import { AUTH } from "./auth.constants.js";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Response, NextFunction } from "express";
+import { AuthenticatedRequest } from "../../shared/types/express.type.js";
+
+export const authMe = asyncHandler(async(req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    const token = req.cookies[AUTH.COOKIE_NAME];
+
+    if (!token) {
+        res.status(401).json({ message: "Não encontrado" });
+        return;
+    }
+
+    try {
+        if (!process.env.JWT_SECRET) 
+            throw new Error("JWT_SECRET não definido!");
+
+        const payload = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+
+        req.token = payload;
+
+        next();
+    } catch {
+        res.clearCookie(AUTH.COOKIE_NAME);
+
+        res.status(401).json({ message: "Token inválido ou expirado" });
+    }
+});
