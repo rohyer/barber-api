e98cc8b58020121e66945a3edf566449448a52ca
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-24 22:35:13 -0300

    refactor auth repository and create findUserByEmail method

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,11 +1,10 @@
 import { Pool, ResultSetHeader, RowDataPacket } from "mysql2/promise";
-import { RegisterBarberShop } from "./auth.dto.js";
-import { AuthEntity, AuthEntityProps } from "./auth.entity.js";
+import { AuthEntity, AuthEntityInput, AuthEntityProps } from "./auth.entity.js";
 
 export class AuthRepository {
     constructor(private readonly db: Pool) {}
 
-    isEmailAvailable = async (email: string): Promise<boolean> => {
+    isEmailRegistered = async (email: string): Promise<boolean> => {
         const query = "SELECT id FROM admin WHERE email = ? LIMIT 1";
 
         const [rows] = await this.db.execute<AuthEntityProps["id"] & RowDataPacket[]>(
@@ -14,12 +13,12 @@ export class AuthRepository {
         );
 
         if (rows.length === 0)
-            return true;
+            return false;
 
-        return false;
+        return true;
     };
 
-    insertBarbershop = async (barbershopData: RegisterBarberShop): Promise<AuthEntity | null> => {
+    insertBarbershop = async (barbershopData: AuthEntityInput): Promise<AuthEntity | null> => {
         const { name, email, password, phone, city, state } = barbershopData;
 
         const query = "INSERT INTO admin (name, email, password, phone, city, state) VALUES (?, ?, ?, ?, ?, ?)";
@@ -32,8 +31,30 @@ export class AuthRepository {
         if (!result.insertId)
             return null;
 
-        const entity = AuthEntity.createFromDatabase({ name, email, password, phone, city, state });
+        const entity = AuthEntity.createFromDatabase({
+            id: result.insertId,
+            name,
+            email,
+            password,
+            phone,
+            city,
+            state,
+        });
 
         return entity;
+    };
+
+    findUserByEmail = async (email: AuthEntityProps["email"]): Promise<AuthEntity | null> => {
+        const [result] = await this.db.execute<AuthEntityProps & RowDataPacket[]>(
+            "SELECT id, name, email, password, city, state, phone, premium_expires_at as premiumExpiresAt from admin WHERE email = ? LIMIT 1",
+            [email],
+        );
+
+        if (result.length === 0)
+            return null;
+
+        const userEntity = AuthEntity.createFromDatabase({ ...result[0] });
+
+        return userEntity;
     };
 }
