4d96331636de058b6f9d869bf4c893431ca83cb2
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2026-04-28 21:45:32 -0300

    change parameter and add name and emailt o token

--- src/modules/auth/jwt.service.ts
+++ src/modules/auth/jwt.service.ts
@@ -1,12 +1,17 @@
 import jwt from "jsonwebtoken";
+import { Barbershop } from "./auth.type.js";
 
 export class JwtService {
-    generateToken = (barbershopId: number) => {
+    generateToken = (barbershop: Barbershop & { id: number }) => {
         if (!process.env.JWT_SECRET) 
             throw new Error("Variável de ambiente não definida");
-    
+
         return jwt.sign(
-            { id: barbershopId },
+            {
+                id: barbershop.id,
+                name: barbershop.name,
+                email: barbershop.email,
+            },
             process.env.JWT_SECRET,
             { expiresIn: "7d" },
         );
